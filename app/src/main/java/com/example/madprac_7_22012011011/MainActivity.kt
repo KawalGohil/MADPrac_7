416b72d60fd1af1142758f3199c7cbc71c1f280f
KawalGohil <kawalgohil@gmail.com>
2024-11-24 19:29:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madprac_7_22012011011/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.madprac_7_22012011011
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.io.Serializable
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+class Person(
+    var id:String,
+    var name:String,
+    var emailId:String,
+    var phoneNo:String,
+    var address:String,
+    var latitude:Double,
+    var longitude:Double):Serializable{
+
+    }
